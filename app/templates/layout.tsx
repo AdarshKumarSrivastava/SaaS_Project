f94431df6186b94f0fd1d302d41eae7dee2d5f86
Adarsh <adarshsrivastava536@gmail.com>
2026-08-31 01:46:30 +0530

    done

--- app/templates/layout.tsx
+++ app/templates/layout.tsx
@@ -1,13 +1,16 @@
 import { PreviewModeButton } from "./PreviewModeButton";
 import { BuilderOverlay } from "@/components/builder/BuilderOverlay";
+import { CustomerAuthProvider } from "@/context/CustomerAuthContext";
 
 export default function TemplatesLayout({ children }: { children: React.ReactNode }) {
   // Fallback to normal layout (for builder preview and template selection)
   return (
     <div className="relative w-full h-full">
-      {children}
-      <PreviewModeButton />
-      <BuilderOverlay />
+      <CustomerAuthProvider siteId="mock_template_site">
+        {children}
+        <PreviewModeButton />
+        <BuilderOverlay />
+      </CustomerAuthProvider>
     </div>
   );
 }
